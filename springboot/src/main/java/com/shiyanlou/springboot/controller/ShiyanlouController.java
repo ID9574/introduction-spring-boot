ef2cdc1af78412394c2db273f68cddd6ac1a0644
ID9574 <roc9574@sina.com>
2019-08-19 16:03:59 +0800

    Spring Boot 多种配置方式

--- springboot/src/main/java/com/shiyanlou/springboot/controller/ShiyanlouController.java
+++ springboot/src/main/java/com/shiyanlou/springboot/controller/ShiyanlouController.java
@@ -1,14 +1,23 @@
 package com.shiyanlou.springboot.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 //RestController相当于同时使用@Controller和@ResponseBody注解
+@PropertySource(value="classpath:shiyanlou.properties")
 public class ShiyanlouController {
+
+    //使用@Value注解注入属性值
+    // @Value("${shiyanlou.springboot}")
+    @Value("${shiyanlou.test}")
+    private String shiyanlou;
 
     @RequestMapping("shiyanlou")
     public String shiyanlou() {
-        return "Hello Shiyanlou";
+        return shiyanlou;
     }
 }
