97a0bd6406f4bf25d65cbc5e060bd4c9ab86b557
ID9574 <roc9574@sina.com>
2019-08-19 16:24:50 +0800

    Spring Boot Web支持

--- springboot/src/main/java/com/shiyanlou/springboot/SpringbootApplication.java
+++ springboot/src/main/java/com/shiyanlou/springboot/SpringbootApplication.java
@@ -3,11 +3,28 @@ package com.shiyanlou.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 // import org.springframework.context.annotation.ImportResource;
+// import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 //通过@ImportResource加载xml配置文件
 // @ImportResource(value = "classpath:config.xml")
 @SpringBootApplication
 public class SpringbootApplication {
+
+    //设置配置类
+    // @Configuration
+    // static class WebConfig implements WebMvcConfigurer {
+
+    //     //重写addResourceHandlers方法
+    //     @Override
+    //     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    //         //设置静态资源映射路径为/**，资源位置为类路径下的upload
+    //         registry.addResourceHandler("/**").addResourceLocations("classpath:/upload/");
+    //     }
+    // }
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
